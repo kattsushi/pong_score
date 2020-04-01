8a47315ad10a344a3ddcfb2cc3b66e1f4b4b2931
Andres J <ndresdavidj@gmail.com>
2020-03-31 18:48:17 -0600

    background

--- lib/presentation/pages/home/home_view_mobile.dart
+++ lib/presentation/pages/home/home_view_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:pong_score/domain/core/layout/base_widget.dart/base_widget_model.dart';
 import 'package:pong_score/domain/core/layout/widgets/drawer_app/drawer_app.dart';
 import 'package:pong_score/presentation/pages/home/home_view_model.dart';
@@ -9,12 +10,21 @@ class HomeMobileViewPortrait extends BaseModelWidget<HomeViewModel> {
   @override
   Widget build(BuildContext context, HomeViewModel model) {
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+    final title = model.title;
     return Container(
       child: Scaffold(
         key: _scaffoldKey,
         drawer: DrawerApp(),
         appBar: HomeMobileViewPortrait.getAppBar(context),
-        body: Container(),
+        body: Container(
+          child: Stack(children: <Widget>[
+            Center(child: Text('hola mundo $title')),
+            Container(child: Image.asset('images/vector.png', width: 100, repeat: ImageRepeat.repeat,), width: 600, height: 800,)
+          ],),
+        ),
+        floatingActionButton: FloatingActionButton(child: Text('add'), onPressed: () {
+          model.updateTitle();
+        },),
       ),
     );
   }
